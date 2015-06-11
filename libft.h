224c82fd5f6a2b80df1531b282904f6e56f07c19
Aymeric Ratinaud <aratinau@student.42.fr>
2015-06-11 21:55:19 +0200

    modif des prototypes en xp_color

--- libft.h
+++ libft.h
@@ -83,7 +83,7 @@ void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void(*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				clear_screen(void);
-void				cl_color(char *s, char *opt);
-void				cl_help(void);
+void				xp_color(char *s, char *opt);
+void				xp_help(void);
 
 #endif
